148dffaf6e4805d10c65e0d359c28fba2e726932
Shubham-2404 <shubhamdarekarr@gmail.com>
2024-06-13 10:52:42 +0530

    Adding main page

---
+++ Main/main.js
@@ -0,0 +1,30 @@
+//! Fetch the data from server
+
+let productInfo = document.querySelector(".pro-container");
+console.log(productInfo);
+
+let getData = async function () {
+  let data = await fetch("./data.json");
+  let finalData = await data.json();
+
+  finalData.map((v, i) => {
+    let { image, title, price } = v;
+    productInfo.innerHTML += ` <div class="pro">
+                <img src=${image} alt="">
+                <div class="des">
+                    <span>addidas</span>
+                    <h5>${title}</h5>
+                    <div class="star"></div>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <h6>$${price}</h6>
+                <a href="#"> <i class="fal fa-shopping-cart cart"></i> </a>
+            </div>`;
+  });
+};
+
+getData();
