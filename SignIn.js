c83a88f6f04a9da54a568dccb159f51c9c6feb26
Shubham-2404 <shubhamdarekarr@gmail.com>
2024-06-25 21:24:07 +0530

    adding sign in page

---
+++ SignIn.js
@@ -0,0 +1,46 @@
+let username = document.querySelector("#userName");
+let password = document.querySelector("#pass");
+let loginbtn = document.querySelector("button");
+
+let getData = JSON.parse(localStorage.getItem("userData"));
+console.log(getData);
+
+loginbtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let currUser = getData.find((user) => {
+    if (
+      (user.userEmail == username.value &&
+        user.userPassword == password.value) ||
+      (user.userMobile == username.value && user.userPassword == password.value)
+    ) {
+      return user;
+    }
+  });
+
+  console.log(currUser);
+
+  if (currUser) {
+    let user = document.querySelector("#welcomemsg");
+    user.innerHTML = `${currUser.userFirstName} welcome to the Shopifyze.`;
+    setTimeout(() => {
+      window.open("./Main.html", "_self");
+    }, 1000);
+  } else if (
+    username.value == "shubhamdarekar@gmail.com" &&
+    password.value == ""
+  ) {
+    alert("Please enter the password.");
+  } else if (
+    username.value == "shubhamdarekar@gmail.com" &&
+    password.value != "shubham@123"
+  ) {
+    alert("Password is incorrect! try again.");
+  } else if (username.value == "" && password.value == "shubham@123") {
+    alert("Please enter the email or mobilenumber.");
+  } else if (username.value == "" && password.value == "") {
+    alert("Please enter the username and password.");
+  } else {
+    alert("Account not found.");
+  }
+});
